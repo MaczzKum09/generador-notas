3c4190ec4cbdd3b2ecdf1d6e8e070c5adb027bf4
MaczzKum09 <maxgarcia.09nov@gmail.com>
2026-01-15 15:53:20 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/generador-notas/", // <--- SOLO AGREGA ESTA LÍNEA AQUÍ
   server: {
     host: "::",
     port: 8080,
